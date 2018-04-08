9a5fcb380f3bd2a7c07352dd287f0cdcf0f60ecc
Sergio Tanzilli <sergio@tanzilli.com>
2018-04-08 17:12:11 +0000

    Esempio base con invio allarmi

--- basic.py
+++ basic.py
@@ -5,89 +5,131 @@
  
 import RPi.GPIO as GPIO
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import telegram
-import logging
-import random
+import telegram,time,os,json
 
-import time
-import os				
-
+#GPIO usato per generale l'allarme da inviare via Telegram (Pin 15)
 P1=22
-P2=27
-P3=17
-P4=4
 
 job_queue=None
 chat_ids=[]
+chat_ids_filename="chat_ids.json"
+alarm_counter=0
 
+#Aggiunge una chat id all'elenco in ram
+def addChatIds(chat_id):
+	global chat_ids
+
+	if chat_id not in chat_ids:
+		chat_ids.append(chat_id)
+
+#Rimuove una chat id all'elenco in ram
+def removeChatIds(chat_id):
+	global chat_ids
+
+	chat_ids.remove(chat_id)
+
+#Salva l'elenco delle chat_id sul file chat_ids.json
+def saveChatIds():
+	global chat_ids
+	global chat_ids_filename
+
+	with open(chat_ids_filename, 'wb') as fp:
+		json.dump(chat_ids, fp)
+		fp.close()	
+
+#Carica l'elenco delle chat_id dal file chat:ids.json
+def loadChatIds():
+	global chat_ids
+	global chat_ids_filename
+
+	if not os.path.isfile(chat_ids_filename):
+		chat_ids=[]
+		return
+
+	with open(chat_ids_filename, 'r') as fp:
+		chat_ids=json.load(fp)
+		fp.close()	
+
+#Handler del comando /start ricevuto da Telegram
+#Il comando /start viene inviato quando da cellulare
+#ci si registra ad un bot
 def cmd_start(bot, update):
-	global chat_ids
-
-	chat_ids+=[update.message.chat_id]
-	bot.sendMessage(update.message.chat_id, text="Hello\n")
+	bot.sendMessage(update.message.chat_id, text="Welcome message\n")
 	print "Ricevuto /start"
-
-def echo(bot, update):
-	print "Mittente: : [" + update.message.from_user.username + "]"
-	print "Testo     : [" + update.message.text + "]"
-
-	bot.sendMessage(update.message.chat_id, text="Ricevuto")
 	
+	#Salva la chat id del messaggio in arrivo per poter
+	#poi usarla per inviare allarmi
+	addChatIds(update.message.chat_id)
+	saveChatIds()
 
-def p1_handler(channel):
-	global job_queue
-	global chat_ids
+#Handler del comando /stop ricevuto da Telegram
+#Il comando /stop viene inviato quando da cellulare
+#ci si cancella ad un bot
+def cmd_stop(bot, update):
+	bot.sendMessage(update.message.chat_id, text="Bye, bye\n")
+	print "Ricevuto /stop"
 
-	if len(chat_ids)>=0:
-		for chat_id in chat_ids:
-			job_queue.bot.sendMessage(chat_id, text="Pressed P1")
+	#Cancella la chat id del messaggio in arrivo per poter
+	#evitare di inviare messaggi di allarme in futuro
+	removeChatIds(update.message.chat_id)
+	saveChatIds()
 
-	print "Pressed P1"
+#Handler di gestione dei messaggi liberi in arrivo
+def echo(bot, update):
+	print "User:   : [" + update.message.from_user.username + "]"
+	print "Text    : [" + update.message.text + "]"
+	bot.sendMessage(update.message.chat_id, text="Ricevuto")
 
-def p2_handler(channel):
-	print "Pressed P2"
+	#Salva la chat id del messaggio in arrivo per poter
+	#poi usarla per inviare allarmi
+	addChatIds(update.message.chat_id)
+	saveChatIds()
 
-def p3_handler(channel):
-	print "Pressed P1"
-
-def p4_handler(channel):
-	print "Pressed P4"
-
-updater = Updater("589601197:AAEWx0L_9G4l3TqDo8YKLgI3PsBu8Xetsm4")	
+#Si registra a Telegram con il Token del bot
+updater = Updater("Inserisci il Token")	
 job_queue = updater.job_queue
 
 dispatcher = updater.dispatcher
 
+#Registra gli handler
 dispatcher.add_handler(CommandHandler("start", cmd_start))
+dispatcher.add_handler(CommandHandler("stop", cmd_stop))
 dispatcher.add_handler(MessageHandler(Filters.text,echo))
 
+# Carica l'elenco delle chat telegram aperte
+loadChatIds()
+
+# Fa partire il gestore dei messaggi Telegram
 update_queue = updater.start_polling()
 print "Started. Type ctrl-C to exit"		
 
+#Configura il pin di allarme (Se a 0 manda l'allarme)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(P1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(P1, GPIO.IN, pull_up_down=GPIO.PUD_UP)	
-GPIO.setup(P2, GPIO.IN, pull_up_down=GPIO.PUD_UP)	
-GPIO.setup(P3, GPIO.IN, pull_up_down=GPIO.PUD_UP)	
-GPIO.setup(P4, GPIO.IN, pull_up_down=GPIO.PUD_UP)	
-
-GPIO.add_event_detect(P1, GPIO.FALLING,p1_handler,200)	
-GPIO.add_event_detect(P2, GPIO.FALLING,p2_handler,200)	
-GPIO.add_event_detect(P3, GPIO.FALLING,p3_handler,200)	
-GPIO.add_event_detect(P4, GPIO.FALLING,p4_handler,200)	
-
+#Loop infinito 
 try:
-	counter=0
 	while True:
-		time.sleep(1)
-		counter=counter+1
-		print counter
+
+		#Controlla se e' stato premuto il tasto di allarme 
+		if GPIO.input(P1)==0:
+			alarm_counter=alarm_counter+1
+			print "Allarme %d" % alarm_counter
+
+			#Invia l'allarme a tutti i cellulari che si sono collegati al bot
+			if len(chat_ids)>=0:
+				for chat_id in chat_ids:
+					job_queue.bot.sendMessage(chat_id, text="Allarme # %d!!" % alarm_counter)
+
+			while GPIO.input(P1)==0:
+				time.sleep(0.2)
+				pass
+	
+		time.sleep(0.2)
+
 finally:
 	print "\nExit in 10 seconds"		
 	updater.stop()
 	GPIO.cleanup()
 	exit()
-
-		
-	
